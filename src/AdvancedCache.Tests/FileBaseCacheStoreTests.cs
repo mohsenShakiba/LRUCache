474eb7729a446b9d9d5d9b580e6b292a923e173f
user <work.shakiba@gmail.comm>
2019-10-19 16:43:54 +0330

    Added test for FileBasedCacheStore

---
+++ src/AdvancedCache.Tests/FileBaseCacheStoreTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AdvancedCache.Tests
+{
+    public class FileBaseCacheStoreTests: IDisposable
+    {
+
+        //private readonly string dummyDirectoryName = "dummy";
+
+        public FileBaseCacheStoreTests()
+        {
+            //Directory.CreateDirectory(dummyDirectoryName);
+        }
+
+        private string DummyPath(string fileName)
+        {
+            return Path.Combine(Path.GetTempPath(), fileName);
+        }
+
+        [Fact]
+        public void NullPath_MustThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new FileBasedCacheStore(null);
+            });
+        }
+
+        [Fact]
+        public void ValidPathEmptyResult_MustReturnValue()
+        {
+            var path = DummyPath("test");
+            var fileBasedCacheStore = new FileBasedCacheStore(path);
+            var cacheEntry = CacheEntry.New("KEY", "VALUE", TimeSpan.FromSeconds(1));
+            fileBasedCacheStore.StoreItems(new CacheEntry[1] { cacheEntry });
+            var restoredItems = fileBasedCacheStore.RestoreItems();
+            var cacheEntryRetrievedFromDist = restoredItems.First();
+
+            Assert.NotNull(restoredItems);
+            Assert.True(restoredItems is IEnumerable<CacheEntry>);
+            Assert.True(restoredItems.Count() == 1);
+
+            Assert.Equal(cacheEntry, cacheEntryRetrievedFromDist);
+            Assert.Equal(cacheEntry.Identifier.Key, cacheEntryRetrievedFromDist.Identifier.Key);
+            Assert.Equal(cacheEntry.Value, cacheEntryRetrievedFromDist.Value);
+            Assert.Equal(cacheEntry.ExpirationPeriod, cacheEntryRetrievedFromDist.ExpirationPeriod);
+        }
+
+        public void Dispose()
+        {
+            //File.Delete(dummyDirectoryName);
+        }
+    }
+}
